d07460caab38b542ccaf88ff274e9d40dbb864ec
Foz3R <pawel.knot.1@gmail.com>
2025-02-08 21:49:22 +0100

    Zamiana dodawania na mnożenie w metodzie Policz

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 
 int Policz(int a, int b)
 {
-    return a + b;
+    return a * b; // Zamiana dodawania na mnożenie
 }
 
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "W pierwszyn branchu; 2 + 3 = " << Policz(2, 3) << std::endl;
+    std::cout << "W drugim branchu; 2 * 3 = " << Policz(2, 3) << std::endl;
     return 0;
 }

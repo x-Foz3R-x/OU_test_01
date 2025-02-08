dc4e88374c3b6686977bcb154bf541e422b17efc
Foz3R <pawel.knot.1@gmail.com>
2025-02-08 21:42:32 +0100

    Nowa metoda Policz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
-int main() {
+int Policz(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "W pierwszyn branchu; 2 + 3 = " << Policz(2, 3) << std::endl;
     return 0;
 }
